878b41cda9a668e4b8d9a094470f307b3e6bd840
Juan Manuel <juanpix2404@gmail.com>
2021-03-01 14:40:20 -0500

    Se remueve parametro innecesario

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ window.onload = function () {
   const mobile_menu = document.querySelector(".mobile-nav");
   const menu_btn = document.querySelector(".hamburger");
 
-  menu_btn.addEventListener("click", function (e) {
+  menu_btn.addEventListener("click", function () {
       menu_btn.classList.toggle("is-active");
       mobile_menu.classList.toggle('is-active')
   });
