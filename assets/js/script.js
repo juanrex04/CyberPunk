a9ac750d3ee22792693e3f9e016b03a4fd819890
Juan Manuel <juanpix2404@gmail.com>
2021-03-01 13:44:02 -0500

    Mejoras en el SCSS para responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.onload = function (){
     window.addEventListener('scroll', function (e){
-        if (window.pageYOffset > 100){
+        if (window.pageYOffset > 90){
             document.querySelector("header").classList.add("is-scrolling");
         } else {
             document.querySelector("header").classList.remove("is-scrolling");
